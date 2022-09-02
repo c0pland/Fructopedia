d76e59426b3f87ad7088123d42e3150edbce9790
Bogdan <c0pland@icloud.com>
2022-09-02 21:23:53 +0300

    007 - Add shuffling and ListView

--- Fructus/FructusApp.swift
+++ Fructus/FructusApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct FructusApp: App {
-    var body: some Scene {
-        WindowGroup {
-            OnboardingView()
-        }
-    }
+	@AppStorage("isOnboarding") var isOnboarding: Bool = true
+	var body: some Scene {
+		WindowGroup {
+			if isOnboarding {
+				OnboardingView()
+			} else {
+				ContentView()
+			}
+		}
+	}
 }
--- Fructus/View/StartButtonView.swift
+++ Fructus/View/StartButtonView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct StartButtonView: View {
+	@AppStorage("isOnboarding") var isOnboarding: Bool?
     var body: some View {
 		Button {
-			print("Exit the onboarding")
+			isOnboarding = false
 		} label: {
 			HStack(spacing: 8) {
-				Text("Start")
+				Text("Learn More")
 				Image(systemName: "arrow.right.circle")
 					.imageScale(.large)
 			}
